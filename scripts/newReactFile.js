e632827bf90aba3db89d7a8aab5456742c59882a
wangjun.byte <wangjun.byte@bytedance.com>
2021-09-17 14:40:21 +0800

    feat: 添加配置

--- scripts/newReactFile.js
+++ scripts/newReactFile.js
@@ -2,7 +2,6 @@ const fs = require('fs');
 const path = require('path');
 const inquirer = require('inquirer');
 const chalk = require('chalk');
-const plop = require('plop');
 
 const newReactFile = async () => {
   const answers = await inquirer.prompt([
@@ -20,22 +19,35 @@ const newReactFile = async () => {
   ]);
 
   if (!answers.fileName) {
-    console.log(chalk.red('文件名必填！'));
+    console.error(chalk.red('文件名必填！'));
     return;
   }
 
   fs.readFile(path.resolve('./templates/reactFile.tpl'), (err, data) => {
     if (err) {
+      console.error('文件读取失败：', err);
       return;
     }
     const contentStr = data.toString();
     const finalStr = contentStr.replace(/{{ NAME }}/g, `${answers.fileName}`);
 
-    fs.writeFile(path.resolve(answers.path) + '.tsx', finalStr, {}, (err) => {
-      console.log('err: ', err);
+    fs.mkdir(answers.path, { recursive: true }, (err) => {
+      if (err) {
+        console.error('创建文件夹失败：', err);
+        throw err;
+      }
+
+      const filePath =
+        path.resolve(answers.path) + '/' + answers.fileName + '.tsx';
+
+      fs.writeFile(filePath, finalStr, {}, (err) => {
+        if (err) {
+          console.error('写入文件失败：', err);
+        }
+        console.log(chalk.green('创建成功，文件位于：', filePath));
+      });
     });
-    console.log(finalStr);
   });
 };
 
-newReactFile();
+module.exports = newReactFile;
